1102392bf8085c1e320d0e437b9eddbc242777fd
german_sotelo <german@cotech.com.co>
2021-08-30 10:37:02 -0500

    creación nuevo proyecto

---
+++ app/src/main/java/com/example/cambiofecha/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.cambiofecha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var fecha = "2021-12-20T16:05:00.121"
+        var fechaCorrecta = ""
+        var b = 'a'
+
+        for (letra in 1..fecha.length) {
+            b = fecha.get(letra - 1)
+
+            when (letra){
+                in 1..10 -> fechaCorrecta = fechaCorrecta+b
+                in 11..13 -> fechaCorrecta = fechaCorrecta
+                in 14..14 -> fechaCorrecta = fechaCorrecta+" "
+                in 15..19 -> fechaCorrecta = fechaCorrecta+b
+
+            }
+
+
+        }
+
+        val fechaView = findViewById<TextView>(R.id.fechaCorrecta)
+        fechaView.text="Su sanción va hasta: $fechaCorrecta"
+
+        println(fechaCorrecta)
+
+    }
+}
